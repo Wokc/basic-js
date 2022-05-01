8a80d5f327deda8d2e2c55c82d8990b770203f2f
Wokc <mozgomozgo93@mail.ru>
2022-05-01 08:07:48 +0300

    common-charachter-count complete

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getCommonCharacterCount(s1, s2) {
+    let arr1 = [];
+    let arr2 = [];
+    if (s1.length > s2.length) {
+        arr1 = s2.split('');
+        arr2 = s1.split('');
+    } else {
+        arr1 = s1.split('');
+        arr2 = s2.split('');
+    }
 
+    let a = arr1.length;
+    let counter = 0;
+    let Sumbol;
+    let b;
+
+    while (a >= 0) {
+        Sumbol = arr1[a];
+        b = arr2.indexOf(Sumbol);
+        console.log(b);
+        if (b != -1) {
+            counter++;
+            arr2.splice(b, 1);
+            console.log(arr2);
+        }
+        a--;
+    }
+    return counter;
+    console.log('Итого:' + counter);
+}
 module.exports = {
-  getCommonCharacterCount
+    getCommonCharacterCount,
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,39 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
+
+let arr = [];
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getLength(arr) {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here}
+        return arr.lenght;
+    },
+    addLink(value) {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here}
+        console.log(value);
+        value = String(value);
+        return arr.push(value);
+    },
+    removeLink(position) {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here}
+        return arr.splice(position, 1);
+    },
+    reverseChain() {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here}
+    },
+    finishChain() {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here}
+        return arr.join('');
+    },
 };
 
 module.exports = {
-  chainMaker
+    chainMaker,
 };
